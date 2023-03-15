347fc5654dc96bfd4dd7b0ca7f616ea55ba9c1cb
Elena <marenina.e@yandex.com>
2023-03-15 13:52:30 +0300

    начало работы над задачами

--- DZ1.py
+++ DZ1.py
@@ -1,1 +1,9 @@
-print(5)
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+number = int(input("Введите трёхзначное число: "))
+digit1 = int(number/100)
+print(digit1)
+digit2 = int(number/10%10)
+print(digit2)
